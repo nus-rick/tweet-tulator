7848c16d7925a37c848f998039190693a8d98294
nusrick <rick@nustechnology.com>
2022-03-22 00:31:43 +0700

    Add script build server

--- api/constants/enviroment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    development: 'development',
-    production: 'production'
-};
-//# sourceMappingURL=enviroment.js.map
--- api/constants/regex.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REX_NUMBER_ONLY = exports.REX_OPERATOR_AND_NUMBER = void 0;
-exports.REX_OPERATOR_AND_NUMBER = /^[\+|\-|\*|\/]\d+$/;
-exports.REX_NUMBER_ONLY = /^\d+$/;
-//# sourceMappingURL=regex.js.map
